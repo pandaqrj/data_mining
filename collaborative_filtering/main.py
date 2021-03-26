4858d0c826f8b4eec8d2108b42bb31d9e1e170c3
panda <pandaqrj@live.com>
2021-03-26 14:11:38 +0800

    Update main.py

--- collaborative_filtering/main.py
+++ collaborative_filtering/main.py
@@ -3,17 +3,16 @@ from data import critics
 
 def sim_distance(person1, person2):
     from math import sqrt
-    si = {}
+    si = [] #交集
     for item in critics[person1]:
         if item in critics[person2]:
-            si[item] = 1
+            si.append(item) #加入交集
 
-    if len(si) == 0: return 0
+    if len(si) == 0: return 0 #交集为空则相似度为0
 
+    #计算欧几里得距离
     sum_of_squares = sum(
-        [
-            pow(critics[person1][item] - critics[person2][item], 2) for item in critics[person1] if item in critics[person2]
-        ]
+        [ pow(critics[person1][item] - critics[person2][item], 2) for item in critics[person1] if item in critics[person2] ]
     )
     return 1/(1+sum_of_squares)
 
